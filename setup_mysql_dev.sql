cb78ea6ccf919d7d1d7ac145780ad361a7708571
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-18 16:04:42 +0100

    added comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+-- creates a database wvn i fit exists it will run without an error
+-- creates a user and it uns without an error even if it exist
+-- It grants all the priviledges to the database and the user 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
