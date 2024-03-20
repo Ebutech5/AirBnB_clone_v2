65801fa1a020ac52a2bd961e2ca3f1aebdf534bb
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-20 15:28:38 +0100

    added users

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents a User for a MySQL database."""
+    __tablename__ = 'users'  # Represents the table name
+    email = Column(String(128), nullable=False)  # Email column, must not be null
+    password = Column(String(128), nullable=False)  # Password column, must not be null
+    first_name = Column(String(128), nullable=True)  # First name column, can be null
+    last_name = Column(String(128), nullable=True)  # Last name column, can be null
