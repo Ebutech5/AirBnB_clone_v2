42ddee4942008002f26cf55de5fa6308aaddffef
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-20 16:47:56 +0100

    fixed db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -23,7 +23,7 @@ class DBStorage:
         HBNB_ENV = os.getenv('HBNB_ENV')
 
         self.__engine = create_engine(f'mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@{HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}', pool_pre_ping=True)
-
+        
         if HBNB_ENV == 'test':
             Base.metadata.drop_all(self.__engine)
 
