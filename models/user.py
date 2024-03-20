fbf1607fa390ebbe5f60844cab0405045019bded
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-20 16:41:34 +0100

    added user

--- models/user.py
+++ models/user.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class User(BaseModel, Base):
-    """Represents a User for a MySQL database."""
-    __tablename__ = 'users'  # Represents the table name
-    email = Column(String(128), nullable=False)  # Email column, must not be null
-    password = Column(String(128), nullable=False)  # Password column, must not be null
-    first_name = Column(String(128), nullable=True)  # First name column, can be null
-    last_name = Column(String(128), nullable=True)  # Last name column, can be null
+class User(Base):
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    # Include other fields and relationships as needed
+
