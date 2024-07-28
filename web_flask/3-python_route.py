615ae0806c2af3ae8716eaef238fb237993105d6
Ebutech5 <cristofarokoye@outlook.com>
2024-07-28 13:40:08 +0100

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,15 +9,18 @@ app = Flask(__name__)
 def index():
     """Return Hello HBNB!"""
     return 'Hello HBNB!'
-
 @app.route('/hbnb', strict_slashes=False)
 def index():
     """Return HBNB"""
     return 'HBNB'
-
 @app.route('/c/<text>', strict_slashes=False)
 def index(text):
     """Display "c" followed by value of text variable"""
     return 'c' + text.replace('_', '')
+@app.route('/python/' defaults={'text': 'is cool'})
+@app.route('/python/<text>' strict_slashes=False)
+def index(text):
+    """Display "Python" followed by value of text variable"""
+    return 'Python' + text.replace('_', '')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
